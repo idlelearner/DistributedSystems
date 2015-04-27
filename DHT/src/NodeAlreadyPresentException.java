2884962aab65fc30cfc57763d49a691181dce1bb
Varun Pandey <varun@x-131-212-225-184.uofm-secure.wireless.umn.edu>
2015-04-27 16:07:25 -0500

    adding explanations for the exception

--- DHT/src/NodeAlreadyPresentException.java
+++ DHT/src/NodeAlreadyPresentException.java
@@ -1,3 +1,9 @@
+/**
+ * exception for when node is already present and we try to re-add it
+ * @author varun
+ *
+ */
+
 public class NodeAlreadyPresentException extends Exception {
 	public NodeAlreadyPresentException() {
 		super ();
--- DHT/src/NodeNotFoundException.java
+++ DHT/src/NodeNotFoundException.java
@@ -1,3 +1,9 @@
+/**
+ * exception for when node with given id is not found in the chord ring
+ * @author varun
+ *
+ */
+
 public class NodeNotFoundException extends Exception {
 	public NodeNotFoundException() {
 		super();
