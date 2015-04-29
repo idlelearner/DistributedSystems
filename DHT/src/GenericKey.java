ae42f4fdb43e4d4a446caad82b2c8e2e2013f919
Dhass <t.bhags21@gmail.com>
2015-04-28 21:55:58 -0500

    - Client to parse the files and insert and search words.

--- DHT/src/GenericKey.java
+++ DHT/src/GenericKey.java
@@ -1,11 +1,14 @@
 /**
- * structure for a generic key (abstract class)
- * word key and node key will extend this class
+ * structure for a generic key (abstract class) word key and node key will
+ * extend this class
+ * 
  * @author varun
  *
  */
 public abstract class GenericKey {
 	public abstract byte[] getByteKey();
+
 	public abstract boolean equals(GenericKey k);
+
 	public abstract String getStringForHashKey();
 }
